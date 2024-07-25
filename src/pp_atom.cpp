0aeefdd3769763df01cd51b421ec25d78f6fc054
janocas <jcasallas2019@gmail.com>
2024-07-25 16:01:24 -0400

    sorting files and move file to archive folder

--- src/pp_atom.cpp
+++ src/pp_atom.cpp
@@ -11,43 +11,34 @@
 #define DATA_SZ 5
 #define NUM_IT 5
 
+std::atomic<bool> atomic_do_swap;
 
-std::mutex g0_mutex;
-//std::mutex g1_mutex;
-std::condition_variable g0_cv;
-//std::condition_variable g1_cv;
-bool g0_ready = false;
-//bool g1_ready = false;
 
 std::vector<int> data_1;
 std::vector<int> data_2;
 
 void receive_data() {
     for (int i = 0; i < data_1.size(); i++) {
-        sleep(1);
+//        sleep(1);
         data_1[i] = i;
         printf("Generating New data in data_1[%d]...\n",data_1[i]);
     }
-    std::unique_lock<std::mutex> ul(g0_mutex);
+    
     printf("wating for processor to swap data....\n");
-    g0_cv.wait(ul,[](){ return g0_ready == false;});
+    atomic_do_swap.load(std::memory_order_acquire);
     data_1.swap(data_2);
-    g0_ready = true;
-    ul.unlock();
-    g0_cv.notify_one();
+    atomic_do_swap .store(false,std::memory_order_release);
+
 }
 
 void send_data() {
-    std::unique_lock<std::mutex> ul(g0_mutex);
-    printf("wating for generator to start to process data....\n");
-    g0_cv.wait(ul,[](){return g0_ready; });
+    
+    printf("wating for generator for data....\n");
     for (int i = 0; i < data_2.size(); i++) {
-        //sleep(1);
+//        sleep(1);
         std::cout <<"the data is: " << data_2[i] << std::endl;
     }
-    g0_ready = false;
-    ul.unlock();
-    g0_cv.notify_one();
+
 
 }
 
@@ -74,6 +65,8 @@ int main () {
 
     data_1.resize(sz);
     data_2.resize(sz);
+    
+    atomic_do_swap.store(true, std::memory_order_release);
 
 
     std::thread generator(data_generator);
